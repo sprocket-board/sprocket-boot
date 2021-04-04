a6e6eb8482b12ca04dfbb215f1f6e048b1e421d5
James Munns <james.munns@ferrous-systems.com>
2021-04-04 23:37:08 +0200

    Close to done with fake flash write, thinking about refactoring

--- src/bin/i2c-statemachine.rs
+++ src/bin/i2c-statemachine.rs
@@ -12,8 +12,8 @@
 // TODO(AJM): Move this comment if/when I move the state machine code somewhere
 // else, like a dedicated crate.
 
-// TODO(AJM): I need some kind of callback interface for doing some reaction
-// when a register read or write is complete.
+// TODO(AJM): What about when we get a stop before we expect?
+// TODO(AJM): Should I be disabling I2C whenever I do a program or write?
 
 #![no_main]
 #![no_std]
@@ -26,6 +26,11 @@ use core::iter::Cycle;
 use cortex_m::peripheral::SCB;
 use sprocket_boot as _; // global logger + panicking-behavior + memory layout
 use smart_leds::RGB;
+use stm32g0xx_hal::flash;
+use stm32g0xx_hal::flash::FlashPage;
+use stm32g0xx_hal::flash::UnlockedFlash;
+use stm32g0xx_hal::flash::WriteErase;
+use stm32g0xx_hal::flash::Error as FlashError;
 use stm32g0xx_hal::gpio::Analog;
 use stm32g0xx_hal::gpio::gpioa::PA11;
 use stm32g0xx_hal::gpio::gpioa::PA12;
@@ -56,6 +61,21 @@ use groundhog_stm32g031::GlobalRollingTimer;
 use stm32g0xx_hal::i2c_periph::{
     I2CPeripheral,
 };
+
+const TOTAL_PAGES: usize = 4;
+
+// TODO: You REALLY can't change this, because I am bit-packing
+const SUBPAGES_PER_PAGE: usize = 8;
+
+const TOTAL_SUBPAGES: usize = TOTAL_PAGES * SUBPAGES_PER_PAGE;
+
+struct PageMap {
+    pages: [u8; TOTAL_PAGES],
+}
+
+// TODO: This should go away. The real device will write at the
+// beginning of flash.
+const PAGE_OFFSET: usize = 32 - TOTAL_PAGES;
 
 fn inner_main() -> Result<(), ()> {
     defmt::info!("Hello, world!");
@@ -132,102 +152,19 @@ fn inner_main() -> Result<(), ()> {
     let mut start = ght.get_ticks();
     let mut ct = 0;
 
-    let mut boot = BootMachine::new(i2c);
+    let mut flash = if let Ok(ulf) = board.FLASH.unlock() {
+        defmt::info!("unlocked.");
+        ulf
+    } else {
+        defmt::error!("Unlock failed!");
+        sprocket_boot::exit()
+    };
+
+    let mut boot = BootMachine::new(i2c, flash);
 
     loop {
         boot.poll().unwrap();
     }
-
-
-    // loop {
-    //     defmt::info!("Waiting for address match...");
-    //     let i2cpac = i2c.borrow_pac();
-
-    //     loop {
-    //         if i2cpac.isr.read().addr().bit_is_set() {
-    //             break;
-    //         }
-    //     }
-
-    //     defmt::info!("Addr match!");
-
-    //     if i2cpac.isr.read().addcode().bits() != 0x69 {
-    //         // panic!("Bad address!");
-    //         return Err(());
-    //     }
-
-    //     // 0: Write transfer, slave enters receiver mode.
-    //     // 1: Read transfer, slave enters transmitter mode.
-    //     if i2cpac.isr.read().dir().bit_is_set() {
-    //         //
-    //         // READ
-    //         //
-
-    //         // TODO: What is TXE? "Optional: Set I2C_ISR.TXE = 1"
-    //         //
-    //         // ISR::TXE
-    //         //
-    //         // This bit is set by hardware when the I2C_TXDR register is empty. It is cleared when the next
-    //         // data to be sent is written in the I2C_TXDR register.
-    //         // This bit can be written to ‘1’ by software in order to flush the transmit data register
-    //         // I2C_TXDR.
-
-    //         // ACK the address
-    //         i2cpac.icr.write(|w| {
-    //             w.addrcf().set_bit()
-    //         });
-
-    //         defmt::info!("Acknowledged addr");
-
-    //         for (i, byte) in buf.iter().enumerate() {
-    //             while i2cpac.isr.read().txis().bit_is_clear() { }
-
-    //             i2cpac.txdr.modify(|_, w| {
-    //                 unsafe {
-    //                     w.txdata().bits(*byte)
-    //                 }
-    //             });
-
-    //             defmt::info!("Sent byte {:?}", i);
-    //         }
-
-    //         while i2cpac.isr.read().txis().bit_is_clear() { }
-
-    //         defmt::info!("Read complete!");
-    //     } else {
-    //         //
-    //         // WRITE
-    //         //
-
-    //         // TODO: What is TXE? "Optional: Set I2C_ISR.TXE = 1"
-    //         //
-    //         // ISR::TXE
-    //         //
-    //         // This bit is set by hardware when the I2C_TXDR register is empty. It is cleared when the next
-    //         // data to be sent is written in the I2C_TXDR register.
-    //         // This bit can be written to ‘1’ by software in order to flush the transmit data register
-    //         // I2C_TXDR.
-
-    //         // ACK the address
-    //         i2cpac.icr.write(|w| {
-    //             w.addrcf().set_bit()
-    //         });
-
-    //         defmt::info!("Acknowledged addr");
-
-    //         for (i, byte) in buf.iter_mut().enumerate() {
-    //             while i2cpac.isr.read().rxne().bit_is_clear() { }
-
-    //             *byte = i2cpac.rxdr.read().rxdata().bits();
-    //             defmt::info!("Received byte {:?}", i);
-    //         }
-
-    //         while i2cpac.isr.read().txis().bit_is_clear() { }
-
-    //         defmt::info!("Write complete!");
-    //     }
-    // }
-    // sprocket_boot::exit();
 }
 
 #[cortex_m_rt::entry]
@@ -252,14 +189,30 @@ struct BootMachine {
     i2c: I2CPeripheral,
     timer: GlobalRollingTimer,
     transfer: Transfer,
-    state: State,
+    state: BootLoadState,
+    action: Option<BootLoadAction>,
     buffer: [u8; 512],
     waiting_for_stop: bool,
     on_read: Option<ReadCompletion>,
     on_write: Option<WriteCompletion>,
+    flash: UnlockedFlash,
+    map: PageMap,
 }
 
-enum State {
+#[derive(Debug, Eq, PartialEq)]
+enum BootLoadAction {
+    EraseThenWrite {
+        erase_page: usize,
+        write_page: usize,
+        write_subpage: u8,
+    },
+    Writing {
+        write_page: usize,
+        write_subpage: u8,
+    }
+}
+
+enum BootLoadState {
     Idle,
     BootloadEnabled {
         ttl_subpages: usize,
@@ -296,27 +249,106 @@ enum TransferDir {
 }
 
 impl BootMachine {
-    fn new(i2c: I2CPeripheral) -> Self {
+    fn new(i2c: I2CPeripheral, flash: UnlockedFlash) -> Self {
         Self {
             i2c,
+            flash,
             buffer: [0u8; 512],
             timer: GlobalRollingTimer::new(),
             transfer: Transfer::Idle,
-            state: State::Idle,
+            state: BootLoadState::Idle,
             waiting_for_stop: false,
             on_read: None,
             on_write: None,
+            action: None,
+            map: PageMap { pages: [0; TOTAL_PAGES] },
         }
     }
 
     fn poll(&mut self) -> Result<(), ()> {
         if self.waiting_for_stop {
             if self.process_stop() {
+                defmt::info!("Got stop!");
+                self.i2c.borrow_pac().cr2.write(|w| {
+                    w.nack().clear_bit()
+                });
                 self.waiting_for_stop = false;
             } else {
                 return Ok(());
             }
         }
+
+        let mut done = false;
+
+        self.action.take().map(|a| {
+            // Disable I2C. We know we just got a stop. Maybe.
+            // NAK everything until we are done
+            unsafe {
+                self.i2c.disable();
+            }
+
+            extern "C" {
+                static _store_start: u32;
+            }
+
+            let store_start = unsafe { &_store_start as *const _ as usize };
+            // let store_page = FlashPage((store_start - flash::FLASH_START) / flash::PAGE_SIZE as usize);
+
+            let action = if let BootLoadAction::EraseThenWrite {
+                erase_page,
+                write_page,
+                write_subpage,
+            } = a {
+                if let Err(e) = self.flash.erase_page(FlashPage(PAGE_OFFSET + erase_page)) {
+                    defmt::error!("Erase failed!");
+
+                    match e {
+                        FlashError::Busy => defmt::info!("busy"),
+                        FlashError::Illegal => defmt::info!("illegal"),
+                        FlashError::EccError => defmt::info!("ecc err"),
+                        FlashError::PageOutOfRange => defmt::info!("page oor"),
+                        FlashError::Failure => defmt::info!("failure"),
+                    }
+
+                    sprocket_boot::exit()
+                }
+
+                // Erase good! Move on to write
+                BootLoadAction::Writing { write_page, write_subpage }
+            } else {
+                a
+            };
+
+            if let BootLoadAction::Writing {
+                write_page,
+                write_subpage,
+            } = action {
+                // NOTE: store_start is already offset to the base of the temporary flash region
+                let store_addr = (store_start + (write_page * flash::PAGE_SIZE as usize))
+                    + (write_subpage as usize * 256);
+                if let Err(e) = self.flash.write(store_addr, &self.buffer[1..257]) {
+                    defmt::error!("Write failed!");
+                    sprocket_boot::exit()
+                }
+            } else {
+                defmt::error!("What?!?!");
+            };
+
+            unsafe {
+                self.i2c.enable();
+            }
+        });
+
+        if done {
+            return Ok(());
+        }
+
+        if !self.i2c.is_enabled() {
+            defmt::error!("Oops, expected to be enabled but we're not");
+            return Err(());
+        }
+
+        // HERE, I need to see if an action is pending, and start acting on it.
 
         let mut old_state = Transfer::Invalid;
         core::mem::swap(&mut self.transfer, &mut old_state);
@@ -336,6 +368,7 @@ impl BootMachine {
                     if let Some(txfr) = self.start_txfr(data) {
                         txfr
                     } else {
+                        defmt::error!("Bad txfr");
                         self.nak();
                         Transfer::Idle
                     }
@@ -367,6 +400,78 @@ impl BootMachine {
         Ok(())
     }
 
+    fn write_page(&mut self, len: usize) {
+        if len != (1 + 256 + 4) {
+            defmt::error!("Wrong number of bytes!");
+            return;
+        }
+        //         * 1: Page/Subpage
+        //             * 0bPPPPP_SSS (32 pages, 8 sub-pages)
+        //             * 0bPPPPP must be <= 23
+        //         * 256: subpage contents
+        //         * 4 byte: For now: 32-bit checksum. Later, CRC32 or Poly1305?
+        let ps = self.buffer[0];
+
+        let page = (ps >> 3) as usize;
+        let subpage = ps & 0b111;
+
+        assert!(page < TOTAL_PAGES);
+        assert!((subpage as usize) < SUBPAGES_PER_PAGE);
+
+        todo!("check checksum!");
+
+        let map = &mut self.map.pages[page];
+
+        if *map == 0 {
+            *map = *map | (1 << subpage);
+            // All subpages have never been written, time to flash
+            self.action = Some(BootLoadAction::EraseThenWrite {
+                erase_page: page,
+                write_page: page,
+                write_subpage: subpage,
+            })
+        } else if (*map & 1 << subpage) == 0 {
+            *map = *map | (1 << subpage);
+            self.action = Some(BootLoadAction::Writing {
+                write_page: page,
+                write_subpage: subpage,
+            });
+        } else {
+            defmt::error!("Can't re-write pages!");
+        }
+
+        //
+    }
+
+    fn activate_bootload(&mut self, len: usize) {
+        if len != 5 {
+            defmt::error!("Wrong number of bytes!");
+            return;
+        }
+
+        let mut checksum_bytes = [0u8; 4];
+        checksum_bytes.copy_from_slice(&self.buffer[..4]);
+
+        let checksum = u32::from_le_bytes(checksum_bytes);
+        let subpages = self.buffer[4];
+
+        if subpages as usize > TOTAL_SUBPAGES {
+            defmt::error!("Too many subpages!");
+            return;
+        }
+
+        // TODO: check we are idle
+        self.state = BootLoadState::BootloadEnabled {
+            ttl_subpages: subpages as usize,
+            checksum,
+        };
+        self.map.pages.iter_mut().for_each(|b| {
+            *b = 0;
+        });
+
+        defmt::info!("Unlocked Bootloading; pages: {=u8}, checksum: 0x{=u32:X}", subpages, checksum);
+    }
+
     fn dummy_read(&mut self) {
         defmt::info!("Dummy read!");
     }
@@ -379,19 +484,28 @@ impl BootMachine {
     // TODO: not bool
     fn process_stop(&mut self) -> bool {
         let i2cpac = self.i2c.borrow_pac();
-        self.nak();
 
-        if i2cpac.isr.read().txis().bit_is_set() {
+        let isr = i2cpac.isr.read();
+
+        // Is the controller asking us for more data still?
+        if isr.txis().bit_is_set() {
+            self.nak();
             defmt::error!("asked for more read when stop expected!");
             i2cpac.txdr.modify(|_, w| {
                 unsafe {
                     w.txdata().bits(0)
                 }
             });
-            // return false;
         }
 
-        // TODO: Check write
+        // Is the controller giving us more data still?
+        if isr.rxne().bit_is_clear() {
+            self.nak();
+            defmt::error!("got write when stop expected!");
+            let _ = i2cpac.rxdr.read().rxdata().bits();
+        }
+
+        // Is the controller finally done?
         if i2cpac.isr.read().stopf().bit_is_set() {
             i2cpac.icr.write(|w| {
                 w.stopcf().set_bit()
@@ -417,7 +531,12 @@ impl BootMachine {
         self.buffer[idx] = i2cpac.rxdr.read().rxdata().bits();
         idx += 1;
 
-        defmt::info!("Wrote byte {:?}/{:?}", idx, len);
+        // Some kind of data race... when this line is uncommented
+        // it causes the host to get a NAK??? But this line is never hit
+        //
+        // TODO: wat
+        //
+        // defmt::info!("Wrote byte {:?}/{:?}", idx, len);
 
         if idx >= len {
             defmt::info!("Done! Waiting for stop");
@@ -469,12 +588,14 @@ impl BootMachine {
     fn start_txfr(&mut self, addr: u8) -> Option<Transfer> {
         match addr {
             // * Write transactions:
-            //     * 0x40 - Start bootload
-            //         * 4 bytes - total checksum (later crc32), little endian
-            //         * 1 byte - total subpages to write
-            //             * NOTE: must be less than 23 * 8 for now
+
+            // * 0x40 - Start bootload
+            //     * 4 bytes - total checksum (later crc32), little endian
+            //     * 1 byte - total subpages to write
+            //         * NOTE: must be less than 23 * 8 for now
             0x40 => {
-                self.on_write = Some(Self::dummy_write);
+                defmt::info!("Start Bootload");
+                self.on_write = Some(Self::activate_bootload);
                 Some(Transfer::Writing {
                     addr,
                     len: 5,
@@ -482,26 +603,38 @@ impl BootMachine {
                 })
             }
 
-            //     * 0x41 - write page command
-            //         * Must have set 0x40 already
-            //         * Always 1 + 1 + 256 + 4 bytes
-            //             * 1: 0x41
-            //             * 1: Page/Subpage
-            //                 * 0bPPPPP_SSS (32 pages, 8 sub-pages)
-            //                 * 0bPPPPP must be <= 23
-            //             * 256: subpage contents
-            //             * 4 byte: For now: 32-bit checksum. Later, CRC32 or Poly1305?
-            //         * On writes to subpage zero: erase page
-            //         * Stretch write ack until checksum + erase + write?
-            //             * I can't use the flash while erasing (or writing?) anyway
-            //         * On writes to 0:0: make sure the reset vector is 0x0800_C000
-            //             * Make sure MSP is maxval?
-            //             * 0:0 must be the last thing written
+            // * 0x41 - write page command
+            //     * Must have set 0x40 already
+            //     * Always 1 + 1 + 256 + 4 bytes
+            //         * 1: 0x41
+            //         * 1: Page/Subpage
+            //             * 0bPPPPP_SSS (32 pages, 8 sub-pages)
+            //             * 0bPPPPP must be <= 23
+            //         * 256: subpage contents
+            //         * 4 byte: For now: 32-bit checksum. Later, CRC32 or Poly1305?
+            //     * On writes to subpage zero: erase page
+            //     * Stretch write ack until checksum + erase + write?
+            //         * I can't use the flash while erasing (or writing?) anyway
+            //     * On writes to 0:0: make sure the reset vector is 0x0800_C000
+            //         * Make sure MSP is maxval?
+            //         * 0:0 must be the last thing written
+            0x41 => {
+                defmt::info!("Write Page");
+                self.on_write = Some(Self::dummy_write);
+                Some(Transfer::Writing {
+                    addr,
+                    len: 1 + 256 + 4,
+                    idx: 0,
+                })
+            }
+
             //     * 0x42 - reboot to new image
             //     * 0x43 - abort bootload
             //         * No data
             //     * 0x44 - Offer image downstream
             //         * Only after finishing 0x40 transaction
+
+
             // * Read transactions
             //     * wr-then-rd 0x10 + 16 bytes => b'sprocket boot!!!'
             0x10 => {
@@ -561,6 +694,11 @@ impl BootMachine {
             defmt::error!("Direction Mismatch!");
             false
         } else {
+            // Clear a NAK
+            i2cpac.cr2.write(|w| {
+                w.nack().clear_bit()
+            });
+
             defmt::info!("Acked a correct address+direction");
             if direction == TransferDir::Read {
                 i2cpac.isr.write(|w| {
